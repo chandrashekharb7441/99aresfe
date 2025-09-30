d1b8c11c3ab1c3993b7ed441cf2e581450408dff
chandrashekharb7441 <140146363+chandrashekharb7441@users.noreply.github.com>
2025-09-30 12:23:28 +0530

    Update UserLogin.js api

--- src/loginAndSignUpComponentsMy/UserLogin.js
+++ src/loginAndSignUpComponentsMy/UserLogin.js
@@ -10,7 +10,7 @@ const UserLogin = () => {
 
   const onSubmit = async (formData) => {
     try {
-      const response = await fetch("http://realestate-vfkm.onrender.com/99acers/user/login", {
+      const response = await fetch("https://realestate-vfkm.onrender.com/99acers/user/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
